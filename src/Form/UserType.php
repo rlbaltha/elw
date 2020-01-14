d90bfcefa64dcb17be78ed6ffb963b090b9ad815
Ron Balthazor <rlbaltha@uga.edu>
2020-01-14 17:12:08 -0500

    mods to user class

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -13,7 +13,8 @@ class UserType extends AbstractType
     {
         $builder
             ->add('username')
-            ->add('roles')
+            ->add('firstname')
+            ->add('lastname')
         ;
     }
 
