9b2d299ac5d2b0bdefab827df88b137e3c41e4eb
Ron Balthazor <rlbaltha@uga.edu>
2020-03-04 17:33:58 -0500

    some progress on LTI

--- src/Controller/LtiController.php
+++ src/Controller/LtiController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Config\Definition\Exception\Exception;
 use IMSGlobal\LTI;
@@ -30,20 +30,22 @@ class LtiController extends AbstractController
     }
 
     /**
-     * @Route("/launch", name="lti_launch", methods={"GET", "POST"})
+     * @Route("/launch", name="lti_launch", methods={"GET", "POST"}, defaults={"activity_id": ""})
      */
-    public function launch($activity_id, Request $request, SessionInterface $session)
+    public function launch($activity_id, Request $request, Session $session)
     {
         if ($request->get('error') != '') {
             die("Problem detected: [".$request->get('error')."] ".$request->get('error_description'));
         }
 
-        try {
-            $launch = LTI\LTI_Message_Launch::new($this->getDatabase($request->get('iss')), new Cache($session), new Cookie($session))
-                ->validate();
-        } catch (LTI\LTI_Exception $err) {
-            die('LTI_Exception: '.$err->getMessage().'. Is your browser blocking third-party cookies?');
-        }
+        $launch = LTI\LTI_Message_Launch::new($this->getDatabase(), new Cache($session), new Cookie($session));
+
+//        try {
+//            $launch = LTI\LTI_Message_Launch::new($this->getDatabase(), new Cache($session), new Cookie($session))
+//                ->validate();
+//        } catch (LTI\LTI_Exception $err) {
+//            die('LTI_Exception: '.$err->getMessage().'. Is your browser blocking third-party cookies?');
+//        }
 
         // assign launch type 'RESOURCE' by default
         $type_launch = LaunchType::RESOURCE;
@@ -56,7 +58,6 @@ class LtiController extends AbstractController
         $data['launch_id'] = $launch->get_launch_id();
 
         $user = User::create_from_launcher($data);
-        $connect_class = $this->getImplementedLTIClass();
 
         // get custom field: activity_id
         if (isset($data['https://purl.imsglobal.org/spec/lti/claim/custom']) &&
@@ -64,17 +65,20 @@ class LtiController extends AbstractController
             $activity_id = $data['https://purl.imsglobal.org/spec/lti/claim/custom']['activity_id'];
         }
 
-        return $connect_class->loginUser($user, $type_launch, $data, $activity_id);
+//        return $connect_class->loginUser($user, $type_launch, $data, $activity_id);
+        // need to develop actual login here
+        return $this->redirectToRoute('course_index');
     }
 
     /**
      * @Route("/login", name="lti_login", methods={"POST", "GET"})
      */
-    public function login(Request $request, SessionInterface $session)
+    public function login(Request $request, Session $session)
     {
+        $issuer = 'https://elwriting.us-east-2.elasticbeanstalk.com';
         $url =  $this->generateUrl('lti_launch', array(), UrlGeneratorInterface::ABSOLUTE_URL);
 
-        LTI\LTI_OIDC_Login::new($this->getDatabase($request->get('iss')), new Cache($session), new Cookie($session))
+        LTI\LTI_OIDC_Login::new($this->getDatabase(), new Cache($session), new Cookie($session))
             ->do_oidc_login_redirect($url)
             ->do_redirect();
     }
@@ -83,29 +87,28 @@ class LtiController extends AbstractController
      * @Route("/jwks", name="lti_jwks", methods={"GET"})
      */
     public function jwks(Request $request) {
-        $router = $this->get('router');
-        $issuer = $request->get('iss');
-        $data = LTI\JWKS_Endpoint::from_issuer($this->getDatabase($issuer), $iss)
-            ->get_public_jwks();
+
+//        simplified database instantiation
+        $issuer = 'https://elwriting.us-east-2.elasticbeanstalk.com';
+        $data = LTI\JWKS_Endpoint::from_issuer($this->getDatabase(), $issuer)->output_jwks();
 
         $response = new JsonResponse();
         $response->setContent(json_encode($data));
         return $response;
     }
 
-    private function getDatabase($issuer) {
-        $implemented_lti_class = $this->getImplementedLTIClass();
-        return new Database($issuer, $implemented_lti_class);
+    private function getDatabase() {
+        return new Database();
     }
 
-    private function getImplementedLTIClass() {
-        $connect_class = $this->getParameter('symfony_lti_class');
-
-        if (empty($connect_class)) {
-            throw new Exception("Connect class is not defined in config packages");
-        }
-
-        return new $connect_class($this->getDoctrine()->getManager());
-    }
+//    private function getImplementedLTIClass() {
+//        $connect_class = $this->getParameter('symfony_lti_class');
+//
+//        if (empty($connect_class)) {
+//            throw new Exception("Connect class is not defined in config packages");
+//        }
+//
+//        return new $connect_class($this->getDoctrine()->getManager());
+//    }
 
 }
--- src/LTI/Util/Database.php
+++ src/LTI/Util/Database.php
@@ -2,80 +2,48 @@
 
 namespace App\LTI\Util;
 
+session_start();
+
 use IMSGlobal\LTI;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Config\Definition\Exception\Exception;
 use App\LTI\Util\LTIConnectAbstract;
 
+$_SESSION['iss'] = [];
+$reg_configs = array_diff(scandir(__DIR__ . '/../Config'), array('..', '.', '.DS_Store'));
+foreach ($reg_configs as $key => $reg_config) {
+    $_SESSION['iss'] = array_merge($_SESSION['iss'], json_decode(file_get_contents(__DIR__ . "/../Config/$reg_config"), true));
+}
+
 /**
  * Class Database
  */
 class Database implements LTI\Database {
-    private $auth_login_url = 'https://elwriting.us-east-2.elasticbeanstalk.com/login';
-    private $auth_token_url = 'https://elwriting.us-east-2.elasticbeanstalk.com/launch';
-    private $key_set_url = 'https://elwriting.us-east-2.elasticbeanstalk.com/jwks';
-    private $client_id = 'from d2l';
-    private $kid = '9d31ac3ead925f3f247a4360c1e617ea';
-    private $issuer = 'https://ugatest2.view.usg.edu';
-    private $private_key = 'https://elwriting.us-east-2.elasticbeanstalk.com/private.key';
-
-    /**
-     * Database constructor.
-     * @param $iss
-     * @param LTIConnectAbstract $connect
-     * @throws Exception
-     */
-    public function __construct($iss, LTIConnectAbstract $connect)
-    {
-        $connect_data = $connect->getDataIssuer($iss);
-
-        $must_keys_in_connect_data = array(
-            'auth_login_url',
-            'auth_token_url',
-            'key_set_url',
-            'client_id',
-            'kid',
-            'private_key'
-        );
-
-        foreach($must_keys_in_connect_data as $key) {
-            if(!isset($connect_data[$key]) || empty($connect_data[$key])) {
-                throw new Exception("Connect data needs this field and can not be empty ".$key);
-            }
-        }
-
-        $this->auth_login_url = $connect_data['auth_login_url'];
-        $this->auth_token_url = $connect_data['auth_token_url'];
-        $this->key_set_url = $connect_data['key_set_url'];
-        $this->client_id = $connect_data['client_id'];
-        $this->kid = $connect_data['kid'];
-        $this->private_key = $connect_data['private_key'];
-        $this->issuer = $iss;
-    }
-
-    /**
-     * @param $iss
-     * @return LTI\LTI_Registration
-     */
     public function find_registration_by_issuer($iss) {
+        if (empty($_SESSION['iss']) || empty($_SESSION['iss'][$iss])) {
+            return false;
+        }
         return LTI\LTI_Registration::new()
-            ->set_auth_login_url($this->auth_login_url)
-            ->set_auth_token_url($this->auth_token_url)
-            ->set_client_id($this->client_id)
-            ->set_key_set_url($this->key_set_url)
-            ->set_kid($this->kid)
-            ->set_issuer($this->issuer)
-            ->set_tool_private_key($this->private_key);
+            ->set_auth_login_url($_SESSION['iss'][$iss]['auth_login_url'])
+            ->set_auth_token_url($_SESSION['iss'][$iss]['auth_token_url'])
+            ->set_auth_server($_SESSION['iss'][$iss]['auth_server'])
+            ->set_client_id($_SESSION['iss'][$iss]['client_id'])
+            ->set_key_set_url($_SESSION['iss'][$iss]['key_set_url'])
+            ->set_kid($_SESSION['iss'][$iss]['kid'])
+            ->set_issuer($iss)
+            ->set_tool_private_key($this->private_key($iss));
     }
 
-    /**
-     * @param $iss
-     * @param $deployment_id
-     * @return LTI\LTI_Deployment
-     */
     public function find_deployment($iss, $deployment_id) {
+        if (!in_array($deployment_id, $_SESSION['iss'][$iss]['deployment'])) {
+            return false;
+        }
         return LTI\LTI_Deployment::new()
             ->set_deployment_id($deployment_id);
+    }
+
+    private function private_key($iss) {
+        return file_get_contents(__DIR__ . $_SESSION['iss'][$iss]['private_key_file']);
     }
 }
 
