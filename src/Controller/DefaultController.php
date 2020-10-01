3f7807632d4b268ff00d06ee23b183cc6271749b
Ron Balthazor <rlbaltha@uga.edu>
2020-10-01 13:42:11 -0400

    lti test 100120

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,15 +3,25 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use OAT\Bundle\Lti1p3Bundle\Security\Authentication\Token\Message\LtiMessageToken;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Security\Core\Security;
 
 class DefaultController extends AbstractController
 {
+    /** @var Security */
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     /**
      * @Route("/home", name="default")
      */
@@ -27,8 +37,20 @@ class DefaultController extends AbstractController
      */
     public function testing()
     {
+        /** @var LtiMessageToken $token */
+        $token = $this->security->getToken();
+
+        // Related registration
+        $registration = $token->getRegistration();
+
+        // Related LTI message
+        $ltiMessage = $token->getLtiMessage();
+
+        // You can even access validation results
+        $validationResults = $token->getValidationResult();
+
         return $this->render('default/index.html.twig', [
-            'lti_message' => "Success",
+            'lti_message' => $ltiMessage,
         ]);
     }
 
