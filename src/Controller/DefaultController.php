9ae74a74e1b724e629fac336bf984bd63413765b
Ron Balthazor <rlbaltha@uga.edu>
2020-10-06 07:37:16 -0400

    lti test 100620_8

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -60,7 +60,7 @@ class DefaultController extends AbstractController
         // You can even access validation results
         $validationResults = $token->getValidationResult();
 
-//        dd($username);
+        dd($email);
 
         return $this->render('default/index.html.twig', [
             'email' => $email,
