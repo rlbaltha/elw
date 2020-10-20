08add6ff47e568caa7efc8652b4e86ca2467c947
Ron Balthazor <rlbaltha@uga.edu>
2020-10-20 15:49:36 -0400

    ags fishing 12

--- src/Controller/LtiController.php
+++ src/Controller/LtiController.php
@@ -159,15 +159,14 @@ class LtiController extends AbstractController
      */
     public function nrps(Request $request)
     {
-        $registration = $this->repository->find($request->get('registration'));
-        dd($registration);
+
         $membership = $this->client->getContextMembership(
             $this->repository->find($request->get('registration')),
             $request->get('url'),
             $request->get('role'),
             intval($request->get('limit'))
         );
-
+        dd($membership);
         return $this->render('lti/nrps.html.twig', [
             'membership' => $membership,
         ]);
