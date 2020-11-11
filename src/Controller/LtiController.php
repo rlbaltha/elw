b0e808e74752c49f2c091c7a7eb3d2414b6fa4b6
Ron Balthazor <rlbaltha@uga.edu>
2020-11-11 16:17:09 -0500

    display lineitems 3

--- src/Controller/LtiController.php
+++ src/Controller/LtiController.php
@@ -202,7 +202,7 @@ class LtiController extends AbstractController
         $options = $this->getHeaderOptions($access_token, $accept_header);
         $response = $this->guzzle->request($method, $uri, $options);
         $data = json_decode($response->getBody()->__toString(), true);
-dd($data);
+
         return $this->render('lti/nrps.html.twig', [
             'membership' => $data,
             'classlists' => $classlists,
@@ -234,7 +234,7 @@ dd($data);
         $options = $this->getHeaderOptions($access_token, $accept_header);
         $response = $this->guzzle->request($method, $uri, $options);
         $data = json_decode($response->getBody()->__toString(), true);
-dd($data);
+
         return $this->render('lti/ags_index.html.twig', [
             'lineitems' => $data,
             'classlists' => $classlists,
