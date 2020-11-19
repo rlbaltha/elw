9ca223b043814742120c36aefa3b7cb68fa9c6e5
Ron Balthazor <rlbaltha@uga.edu>
2020-11-19 13:06:58 -0500

    score form 24

--- src/Form/LtiAgsScoreType.php
+++ src/Form/LtiAgsScoreType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class LtiAgsScoreType extends AbstractType
 {
