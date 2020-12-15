f79f8e4bfc3b749ad5006c1f80157fcadacc4847
Ron Balthazor <rlbaltha@uga.edu>
2020-12-15 14:15:05 -0500

    security on markupsets

--- src/Controller/MarkupsetController.php
+++ src/Controller/MarkupsetController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 /**
  * @Route("/markupset")
@@ -90,6 +91,10 @@ class MarkupsetController extends AbstractController
      */
     public function edit(Request $request, Markupset $markupset, AuthorizationCheckerInterface $authorizationChecker): Response
     {
+        if (!($this->getUser() == $markupset->getUser() or $this->isGranted('ROLE_ADMIN'))) {
+            throw new AccessDeniedException('You do not have permissions to do this!');
+        }
+
         $form = $this->createForm(MarkupsetType::class, $markupset);
         $form->handleRequest($request);
 
