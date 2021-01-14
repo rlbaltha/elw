b891f0fc11688c776bdf8c5be90d5e2fd03d8db5
Ron Balthazor <rlbaltha@uga.edu>
2021-01-14 11:16:16 -0500

    homepage variables

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -13,12 +13,13 @@ class DefaultController extends AbstractController
      */
     public function index()
     {
+        $login_access = $_ENV['LOGIN_ACCESS'];
+        $login_note = $_ENV['LOGIN_NOTE'];
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
+            'login_access' => $login_access,
+            'login_note' => $login_note,
+
         ]);
     }
-
-
-
-
 }
